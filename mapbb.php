faaee279ec40b3681eb5ab7b7fc6a9f90e618102
Alexander Sapozhnikov <shoorick@mail.ru>
2013-11-05 02:31:17 +0600

    Shortcode sample copied from http://codex.wordpress.org/Shortcode_API

--- mapbb.php
+++ mapbb.php
@@ -6,5 +6,13 @@ Description: Allows use [map] codes for Leaflet maps embedding info pages
 Version: 0.1
 Author: Alexander Sapozhnikov
 Author URI: http://shoorick.ru/
-*/
+ */
+
+// [map]
+function map_process( $atts ){
+    return "(Here we will put map)";
+}
+
+add_shortcode( 'map', 'map_process' );
+
 ?>
