dc00995141c4e19f87c9d949d51123f808b9b803
Alexander Sapozhnikov <shoorick@mail.ru>
2013-11-05 03:02:33 +0600

    Attribute and content processing added

--- mapbb.php
+++ mapbb.php
@@ -6,11 +6,15 @@ Description: Allows use [map] codes for Leaflet maps embedding info pages
 Version: 0.1
 Author: Alexander Sapozhnikov
 Author URI: http://shoorick.ru/
- */
+*/
 
 // [map]
-function map_process( $atts ){
-    return "(Here we will put map)";
+function map_process( $atts, $content = NULL ){
+
+    if (!empty($atts[0]))
+        $attr = preg_replace('/^=/', '', array_shift($atts));
+
+    return "<p style=\"background:#fec\">content: $content<br>attribute: $attr</p>";
 }
 
 add_shortcode( 'map', 'map_process' );
