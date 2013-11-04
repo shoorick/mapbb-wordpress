0d30aa77f58f28c0ce3fad5ebb0276e9d05a43b3
Alexander Sapozhnikov <shoorick@mail.ru>
2013-11-05 01:21:26 +0600

    Stub added

---
+++ mapbb.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: MapBBCode for WordPress
+Plugin URI: http://shoorick.ru/software/mapbb-wordpress
+Description: Allows use [map] codes for Leaflet maps embedding info pages
+Version: 0.1
+Author: Alexander Sapozhnikov
+Author URI: http://shoorick.ru/
+*/
+?>
