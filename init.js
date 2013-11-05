549432be086a5a4a288db4ac6b2688aedba3eca8
Alexander Sapozhnikov <shoorick@mail.ru>
2013-11-05 13:23:44 +0600

    MapBBCode init script added as separate file

---
+++ init.js
@@ -0,0 +1,14 @@
+var mapBBcode = new MapBBCode({
+    windowPath: 'mapbbcode/',
+    layers: 'OpenStreetMap',
+    defaultZoom: 5,
+    defaultPosition: [55, 61],
+    viewWidth: 604,
+    viewHeight: 300,
+    fullViewHeight: 600,
+    editorHeight: 400,
+    windowWidth: 800,
+    windowHeight: 500,
+    fullFromStart: false,
+    preferStandardLayerSwitcher: true
+});
