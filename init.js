60f72a52607830d38e984dd2e0891993ba61caf4
Alexander Sapozhnikov <shoorick@mail.ru>
2013-11-05 16:48:39 +0600

    Layers control added. Default layer is OpenMapSurfer.

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 var mapBBcode = new MapBBCode({
     windowPath: 'mapbbcode/',
-    layers: 'OpenStreetMap',
+//    layers: 'OpenStreetMap',
     defaultZoom: 5,
     defaultPosition: [55, 61],
     viewWidth: 604,
@@ -10,5 +10,9 @@ var mapBBcode = new MapBBCode({
     windowWidth: 800,
     windowHeight: 500,
     fullFromStart: false,
-    preferStandardLayerSwitcher: true
+    preferStandardLayerSwitcher: true,
+    createLayers: function(L) { return [
+        L.tileLayer('http://129.206.74.245:8001/tms_r.ashx?x={x}&y={y}&z={z}', { name: 'OpenMapSurfer', attribution: 'Map &copy; <a href=\"http://openstreetmap.org\">OSM</a> | Tiles &copy; <a href=\"http://giscience.uni-hd.de/\">GIScience Heidelberg</a>', minZoom: 0, maxZoom: 19 }),
+        MapBBCode.prototype.createOpenStreetMapLayer()
+    ]}
 });
